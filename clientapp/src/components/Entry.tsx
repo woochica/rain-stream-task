9db4b91c345c78704e3f8d4d405c9174fdb2471e
Gabor Torok <gabor@20y.hu>
2024-01-20 20:16:09 +0100

    Format JS files

--- clientapp/src/components/Entry.tsx
+++ clientapp/src/components/Entry.tsx
@@ -1,25 +1,44 @@
 type Props = {
-  createdAt: string, url: string, content: string, avatar: string, displayName: string
+  createdAt: string;
+  url: string;
+  content: string;
+  avatar: string;
+  displayName: string;
 };
 
-export const Entry = ({createdAt, url, content, avatar, displayName}: Props) => {
-    const markup = { __html: content };
-    return (
-      <div className="max-w-sm w-full lg:max-w-full lg:flex">
-        <div className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+export const Entry = ({
+  createdAt,
+  url,
+  content,
+  avatar,
+  displayName,
+}: Props) => {
+  const markup = { __html: content };
+  return (
+    <div className="max-w-sm w-full lg:max-w-full lg:flex">
+      <div className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
         <div className="mb-8">
           <a href={url} target="_blank" rel="external nofollow">
-            <div className="text-gray-900 text-xl mb-2 text-left" dangerouslySetInnerHTML={markup}></div>
+            <div
+              className="text-gray-900 text-xl mb-2 text-left"
+              dangerouslySetInnerHTML={markup}
+            ></div>
           </a>
         </div>
         <div className="flex items-center">
-          <img className="w-10 h-10 rounded-full mr-4" src={avatar} alt={displayName} />
+          <img
+            className="w-10 h-10 rounded-full mr-4"
+            src={avatar}
+            alt={displayName}
+          />
           <div className="text-sm">
-            <p className="text-gray-900 leading-none text-left">{displayName}</p>
+            <p className="text-gray-900 leading-none text-left">
+              {displayName}
+            </p>
             <p className="text-gray-600">{createdAt}</p>
           </div>
         </div>
       </div>
     </div>
-    );
+  );
 };
--- clientapp/src/components/EntryList.tsx
+++ clientapp/src/components/EntryList.tsx
@@ -1,9 +1,9 @@
-import React, {useCallback, useState} from 'react';
-import {useStream} from 'react-fetch-streams';
-import {Entry} from './Entry';
+import React, { useCallback, useState } from "react";
+import { useStream } from "react-fetch-streams";
+import { Entry } from "./Entry";
 
-const serverUrl = 'http://localhost:8000/public';
-const fetchParams = {mode: 'cors'}
+const serverUrl = "http://localhost:8000/public";
+const fetchParams = { mode: "cors" };
 
 type ResponseEntry = {
   id: string;
@@ -18,28 +18,32 @@ type ResponseEntry = {
 
 export const EntryList = () => {
   const [entries, setEntries] = useState<ResponseEntry[]>([]);
-  const onNext = useCallback(async (res: Response) => {
-        // TODO add schema validation
-        const newEntry = await res.json() as unknown as ResponseEntry;
-        setEntries([newEntry, ...entries]);
-        console.debug('new entry', newEntry);
-    }, [setEntries, entries]);
-  useStream(serverUrl, {onNext, fetchParams});
+  const onNext = useCallback(
+    async (res: Response) => {
+      // TODO add schema validation
+      const newEntry = (await res.json()) as unknown as ResponseEntry;
+      setEntries([newEntry, ...entries]);
+      console.debug("new entry", newEntry);
+    },
+    [setEntries, entries],
+  );
+  useStream(serverUrl, { onNext, fetchParams });
 
-    const entriesContent = entries.map(entry => (
-            <Entry key={entry.id}
-                avatar={entry.account.avatar}
-                displayName={entry.account.display_name}
-                createdAt={entry.created_at}
-                content={entry.content}
-                url={entry.url}
-                />
-        ));
+  const entriesContent = entries.map((entry) => (
+    <Entry
+      key={entry.id}
+      avatar={entry.account.avatar}
+      displayName={entry.account.display_name}
+      createdAt={entry.created_at}
+      content={entry.content}
+      url={entry.url}
+    />
+  ));
 
-    return (
-      <div className="mb-32 grid gap-8 text-center">
-        <h1 className="mb-3 text-2xl font-semibold">Recent public entries</h1>
-        {entries.length > 0 ? entriesContent : <p>No entries yet.</p>}
-      </div>
-    );
+  return (
+    <div className="mb-32 grid gap-8 text-center">
+      <h1 className="mb-3 text-2xl font-semibold">Recent public entries</h1>
+      {entries.length > 0 ? entriesContent : <p>No entries yet.</p>}
+    </div>
+  );
 };
